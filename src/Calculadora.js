321a478187c7955a83e7d54f76e4e4c3338434c2
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-07-14 17:18:18 -0300

    adicionado funcionalidades ao componente principal

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,23 +1,66 @@
 import React, { useState } from "react";
 import "./Calculadora.css";
 import { Jumbotron, Container, Row, Col, Button, Form } from "react-bootstrap";
+import CalculadoraService from "./components/Calculadora.service";
 
 function Calculadora() {
-    const [txtNumeros, setTxtNumeros] = useState("0");
+    // eslint-disable-next-line no-unused-vars
+    const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
+
+    const [txtNumeros, setTxtNumeros] = useState("0"); //estado do campo de texto
+    const [numero1, setNumero1] = useState("0"); //estado do primeiro numero digitado
+    const [numero2, setNumero2] = useState(null); //estado do segundo numero digitado
+    const [operacao, setOperacao] = useState(null); //estado da operacao digitada
 
     function adicionarNumero(numero) {
-        setTxtNumeros(txtNumeros + numero);
+        let resultado;
+        if (operacao === null) {
+            resultado = concatenarNumero(numero1, numero);
+            setNumero1(resultado);
+        } else {
+            resultado = concatenarNumero(numero2, numero);
+            setNumero2(resultado);
+        }
+        setTxtNumeros(resultado);
     }
 
     function definirOperacao(op) {
-        setTxtNumeros(op);
+        //apenas define operacao caso nao exista
+        if (operacao === null) {
+            setOperacao(op);
+            setTxtNumeros(op);
+            return;
+        }
+        //caso operacao estiver definida e numero 2 selecionado, realiza o calculo da operacao previa
+        if (operacao !== null) {
+            const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+            setOperacao(op);
+            setNumero1(resultado.toString());
+            setNumero2(null);
+            setTxtNumeros(resultado.toString());
+        }
+    }
+
+    function acaoCalcular() {
+        if (numero2 === null) {
+            return;
+        }
+        const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+        setTxtNumeros(resultado.toString());
+    }
+
+    function limpar() {
+        setTxtNumeros("0");
+        setNumero1("0");
+        setNumero2(null);
+        setOperacao(null);
     }
 
     return (
         <Jumbotron
             style={{
                 background: "transparent !important",
-                backgroundColor: "#007bff",
+                backgroundColor: "#4c4c4c",
                 padding: "5px",
                 margin: "20px auto",
                 width: "400px",
@@ -27,7 +70,9 @@ function Calculadora() {
             <Container>
                 <Row>
                     <Col xs="3">
-                        <Button variant="danger">C</Button>
+                        <Button variant="danger" onClick={limpar}>
+                            C
+                        </Button>
                     </Col>
 
                     <Col xs="9">
@@ -117,10 +162,14 @@ function Calculadora() {
                         </Button>
                     </Col>
                     <Col>
-                        <Button variant="light">.</Button>
+                        <Button variant="light" onClick={() => adicionarNumero(".")}>
+                            .
+                        </Button>
                     </Col>
                     <Col>
-                        <Button variant="success">=</Button>
+                        <Button variant="success" onClick={acaoCalcular}>
+                            =
+                        </Button>
                     </Col>
                     <Col>
                         <Button variant="warning" onClick={() => definirOperacao("+")}>
